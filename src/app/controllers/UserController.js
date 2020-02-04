f5cf287ef8107472f9f44c9d74af5a6e13ec48bb
Matheus Henrique Passos de Oliveira <matheus.passos@ifgoiano.edu.br>
2020-02-04 12:40:42 -0300

    validate user creation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,37 @@
+import * as yup from 'yup';
 import User from '../models/User';
 import Group from '../models/Group';
 
 class UserController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      id: yup
+        .number()
+        .integer()
+        .positive()
+        .integer()
+        .required(),
+      name: yup.string().required(),
+      email: yup
+        .string()
+        .email()
+        .required(),
+      password: yup
+        .string()
+        .required()
+        .min(8),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(400)
+        .json({ errorCode: -6, errorMessage: 'Input request is invalid' });
+    }
+    const userExists = await User.findOne({ where: { id: req.body.id } });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ errorCode: -7, errorMessage: 'User already exists' });
+    }
     const user = await User.create(req.body);
 
     return res.json(user);
